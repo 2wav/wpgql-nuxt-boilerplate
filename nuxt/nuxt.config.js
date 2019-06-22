d7b6c886c8a1a3d82fe544fc0e7095003114b9a9
adrianbw <adrian@2wav.com>
2019-06-22 14:15:55 -0500

    Config WIP

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,25 +1,33 @@
+/*
+ ** Polyfills that we find to be frequently needed
+ */
+const features = ["fetch", "Object.assign", "Object.entries"].join("%2C")
+
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
+    script: [
+      { src: `https://polyfill.io/v3/polyfill.min.js?features=${features}` }
+    ],
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -33,9 +41,10 @@ module.exports = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/eslint-module'
+    "@nuxtjs/axios",
+    "@nuxtjs/apollo",
+    "@nuxtjs/pwa",
+    "@nuxtjs/eslint-module"
   ],
   /*
    ** Axios module configuration
