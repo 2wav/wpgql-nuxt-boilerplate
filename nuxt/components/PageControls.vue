13b616d868d48f51f3df0bfd80a90ceb7663e63a
adrianbw <adrian@2wav.com>
2019-06-24 12:59:40 -0500

    Add page control buttons

---
+++ nuxt/components/PageControls.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="page-controls">
+    <button
+      class="page-controls__button page-controls__button--previous button--green"
+      @click="clickedPrevious"
+    >
+      Previous Page
+    </button>
+    <button
+      class="page-controls__button page-controls__button--next button--green"
+      @click="clickedNext"
+    >
+      Next Page
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    hasPreviousPage: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    },
+    hasNextPage: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    }
+  },
+  methods: {
+    clickedPrevious() {
+      this.$emit("clickedPrevious");
+    },
+    clickedNext() {
+      this.$emit("clickedNext");
+    }
+  }
+};
+</script>
