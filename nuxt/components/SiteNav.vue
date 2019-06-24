fdc1a0e8cc74dbc39f8b4fc715dee01a57389cd9
adrianbw <adrian@2wav.com>
2019-06-24 14:56:33 -0500

    Show active page

--- nuxt/components/SiteNav.vue
+++ nuxt/components/SiteNav.vue
@@ -3,7 +3,8 @@
     <a
       v-for="item in siteNav"
       :key="item.id"
-      class="site-nav__item button--green"
+      class="site-nav__item site-nav__button site-nav__button--green"
+      :class="{ 'site-nav__button--active': activeItem(item.label) }"
       :href="item.url"
       v-html="item.label"
     />
@@ -11,6 +12,8 @@
 </template>
 
 <script>
+import { get } from "lodash";
+
 import GetMenuBySlug from "~/apollo/queries/getMenuBySlug";
 export default {
   apollo: {
@@ -22,6 +25,20 @@ export default {
       update(data) {
         return data.menus.nodes[0].menuItems.nodes;
       }
+    }
+  },
+  methods: {
+    activeItem(label) {
+      const path = get(this.$route, "path");
+      const lowerCaseLabel = label.toLowerCase();
+      if (path) {
+        if (lowerCaseLabel !== "home") {
+          return `/${lowerCaseLabel}` === path;
+        } else {
+          return path === "/";
+        }
+      }
+      return false;
     }
   }
 };
