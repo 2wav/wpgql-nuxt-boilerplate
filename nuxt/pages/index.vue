885c8da988a483e6902adcf5245acad579626a8b
adrianbw <adrian@2wav.com>
2019-06-24 14:57:56 -0500

    Use mixins

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -8,15 +8,25 @@
       <h2 class="subtitle">
         A boilerplate Nuxt app that gets data from Wordpress through GraphQL
       </h2>
+      <site-nav />
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
+        <a
+          href="https://github.com/2wav/wpgql-nuxt-boilerplate"
+          target="_blank"
+          class="button button--blue"
+          >Boilerplate Documentation</a
+        >
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button button--green"
+          >Nuxt Documentation</a
         >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey"
-          >GitHub</a
+          class="button button--blue"
+          >Nuxt GitHub</a
         >
       </div>
     </div>
@@ -24,17 +34,19 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
+import Logo from "~/components/Logo";
+import SiteNav from "~/components/SiteNav";
 
 export default {
   components: {
-    Logo
+    Logo,
+    SiteNav
   },
-  layout: "frontpage"
+  layout: "frontPage"
 };
 </script>
 
-<style>
+<style lang="scss">
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -48,7 +60,7 @@ export default {
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: get-color("blue");
   word-spacing: 5px;
   padding-bottom: 15px;
 }
